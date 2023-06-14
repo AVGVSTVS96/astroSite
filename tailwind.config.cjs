cb3ebae3a12f84e5a6731e967f8a79d2a9d04bf4
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-06-14 00:59:33 -0400

    add blue and gray color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
+		extend: {
+			colors: {
+				chocolate: {
+					100: '#D2691E',
+					200: '#CD661D',
+				},
+				brown: {
+					100: '#804D3B',
+					200: '#522E24'
+				},
+				blueish: {
+					100: '#9AEDF4',
+					200: '#75BEDA'
+				},
+			},
+			plugins: [
+				require('@tailwindcss/typography')
+			],
+		}
+	}
 }
