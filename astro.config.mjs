93d073aa353482455bbd68b0258a40418d4d4b8e
Bassim <bassim5n@gmail.com>
2023-12-19 13:44:31 -0500

    Add markdown syntax highlighting theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'material-theme-ocean'
+    }
+  },
   integrations: [tailwind()]
 });
