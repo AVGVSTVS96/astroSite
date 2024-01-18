25dd554d088c5b6f575ccc981309dc762adaf02f
Bassim <bassim5n@gmail.com>
2024-01-17 20:10:17 -0500

    Add bg and text theme colors, replace bg and text colors with new custom color classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
@@ -8,7 +11,18 @@ module.exports = {
 				mycustom: {
 					100: '#15162e',
 					200: '#191D2D'
-				  }
+				  },
+
+				dark: {
+					text: colors.slate[200],
+					bg: colors.slate[950],
+					
+				},
+
+				light: {
+					text: colors.slate[900],
+					bg: colors.slate[50],
+				}
 			},
 		}
 	},
