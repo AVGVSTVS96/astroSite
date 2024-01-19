7a8b7f0cfdcb797c3affa0152835ea3282bf145a
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-01-19 00:50:09 -0500

    Delete comments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,17 +12,6 @@ module.exports = {
 					100: '#15162e',
 					200: '#191D2D'
 				},
-
-				// dark: {
-				// 	text: colors.slate[200],
-				// 	bg: colors.slate[950],
-
-				// },
-
-				// light: {
-				// 	text: colors.slate[900],
-				// 	bg: colors.slate[50],
-				// }
 			},
 
 			textColor: {
@@ -41,12 +30,10 @@ module.exports = {
 			},
 
 			borderColor: {
-				// Used in BlogIndex.astro, Footer.astro, NavBar.astro
 				slate: {
 					DEFAULT: colors.slate[400], // slate-400 = #64748b
 				},
 				light: colors.slate[200], // slate-200 = #d1d5db
-				// Used in Projects.astro for tag borders and MDLayout, Card for HR
 				'light-accent': colors.sky[200], // sky-200 = #bae6fd
 				accent: colors.sky[400], // sky-400 = #60a5fa
 				'dark-accent': colors.sky[500], // sky-500 = #3b82f6
