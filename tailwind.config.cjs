466f5d4f0689da1aa1efc41d93e74bff4d42bcd3
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-01-23 21:45:57 -0500

    add vars to config, add rgb values to css, add ThemeSwitcher

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -28,18 +28,18 @@ module.exports = {
 					950: colors[primaryColor][950]
 				},
 				accent: {
-					50: colors[accentColor][50],
-					100: colors[accentColor][100],
-					200: colors[accentColor][200],
-					300: colors[accentColor][300],
-					400: colors[accentColor][400],
-					500: colors[accentColor][500],
-					600: colors[accentColor][600],
-					700: colors[accentColor][700],
-					800: colors[accentColor][800],
-					900: colors[accentColor][900],
-					950: colors[accentColor][950]
-				},
+					50: 'rgb(var(--accent-50) / <alpha-value>)',
+					100: 'rgb(var(--accent-100) / <alpha-value>)',
+					200: 'rgb(var(--accent-200) / <alpha-value>)',
+					300: 'rgb(var(--accent-300) / <alpha-value>)',
+					400: 'rgb(var(--accent-400) / <alpha-value>)',
+					500: 'rgb(var(--accent-500) / <alpha-value>)',
+					600: 'rgb(var(--accent-600) / <alpha-value>)',
+					700: 'rgb(var(--accent-700) / <alpha-value>)',
+					800: 'rgb(var(--accent-800) / <alpha-value>)',
+					900: 'rgb(var(--accent-900) / <alpha-value>)',
+					950: 'rgb(var(--accent-950) / <alpha-value>)',
+				  }, 
 			},
 
 			borderColor: {
