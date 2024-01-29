f4deeccdbc5cbaa9b323d67196b9e93ab0c83f4e
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-01-28 21:47:09 -0500

    Convert astro config file to typescript

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import remarkSectionize from 'remark-sectionize';
-import expressiveCode from 'astro-expressive-code';
+import expressiveCode, { type AstroExpressiveCodeOptions } from 'astro-expressive-code';
 import icon from 'astro-icon';
 
-/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
-const astroExpressiveCodeOptions = {
+const astroExpressiveCodeOptions:AstroExpressiveCodeOptions = {
   themes: ['material-theme-ocean', 'material-theme-palenight'],
   themeCssSelector: (theme) => `[data-code-theme='${theme.name}']`
 };
