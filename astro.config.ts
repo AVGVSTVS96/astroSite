3309b56f164bc93cd7c9f45c70df1816aec11bb2
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-01-28 22:17:36 -0500

    Refactor astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import remarkSectionize from 'remark-sectionize';
-import expressiveCode, { type AstroExpressiveCodeOptions } from 'astro-expressive-code';
+import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import expressiveCode, {
+  type AstroExpressiveCodeOptions,
+} from 'astro-expressive-code';
 
-const astroExpressiveCodeOptions:AstroExpressiveCodeOptions = {
+const astroExpressiveCodeOptions: AstroExpressiveCodeOptions = {
   themes: ['material-theme-ocean', 'material-theme-palenight'],
-  themeCssSelector: (theme) => `[data-code-theme='${theme.name}']`
+  themeCssSelector: (theme) => `[data-code-theme='${theme.name}']`,
 };
 
 // https://astro.build/config
