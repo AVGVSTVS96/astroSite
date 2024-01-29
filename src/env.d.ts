e081c9473de63f41aa39067aa4d0ac2bf81f2cb5
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-01-28 22:09:45 -0500

    Add type declaration for 'remark-sectionize' module

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,16 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module 'remark-sectionize' {
+  import { Node } from 'unist';
+
+  type Transformer = (tree: Node) => void;
+
+  interface Plugin {
+    (): Transformer;
+  }
+
+  const remarkSectionize: Plugin;
+
+  export default remarkSectionize;
+}
