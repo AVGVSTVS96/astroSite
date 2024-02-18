ca8672382ba6a8ed5270a2ce5a56d1769d90434b
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-02-17 21:07:39 -0500

    style: update code theme and selection colors

--- astro.config.ts
+++ astro.config.ts
@@ -7,8 +7,15 @@ import expressiveCode, {
 } from 'astro-expressive-code';
 
 const astroExpressiveCodeOptions: AstroExpressiveCodeOptions = {
-  themes: ['material-theme-palenight', 'material-theme-ocean'],
+  themes: ['rose-pine-moon', 'rose-pine'],
   themeCssSelector: (theme) => `.${theme.type}`,
+  useThemedSelectionColors: true,
+  styleOverrides: {
+    textMarkers: {
+      markBackground: 'hsla(220, 25.00%, 30%, 0.4)',
+      markBorderColor: 'hsla(220, 30.00%, 30%, 1)',
+    }
+  },
 };
 
 // https://astro.build/config
