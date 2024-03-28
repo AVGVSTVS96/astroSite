9dac6185e790717294b60037e14493b1bf893953
AVGVSTVS96 <bassim101@gmail.com>
2024-03-28 13:53:49 -0400

    Wrap menu sections in sections tags

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -23,68 +23,72 @@ export function SideMenu() {
       <SheetContent side="left" className="w-64">
         <SheetTitle className="font-bold">bassim</SheetTitle>
         <nav className="mt-4 flex flex-col *:justify-start">
-          <span className="ml-2 flex items-center">
-            <HomeIcon className="size-4" />
-            <Button
-              asChild
-              variant="link"
-              size="lg"
-              className="px-3 hover:no-underline">
-              <a href="/">Home</a>
-            </Button>
-          </span>
-          <span className="ml-2 flex items-center">
-            <User className="size-4" />
-            <Button
-              asChild
-              variant="link"
-              size="lg"
-              className="px-3 hover:no-underline">
-              <a href="/about">About</a>
-            </Button>
-          </span>
-          <span className="ml-2 flex items-center">
-            <NotebookText className="size-4" />
-            <Button
-              asChild
-              variant="link"
-              size="lg"
-              className="px-3 hover:no-underline">
-              <a href="/blog">Blog</a>
-            </Button>
-          </span>
+          <section>
+            <span className="ml-2 flex items-center">
+              <HomeIcon className="size-4" />
+              <Button
+                asChild
+                variant="link"
+                size="lg"
+                className="px-3 hover:no-underline">
+                <a href="/">Home</a>
+              </Button>
+            </span>
+            <span className="ml-2 flex items-center">
+              <User className="size-4" />
+              <Button
+                asChild
+                variant="link"
+                size="lg"
+                className="px-3 hover:no-underline">
+                <a href="/about">About</a>
+              </Button>
+            </span>
+            <span className="ml-2 flex items-center">
+              <NotebookText className="size-4" />
+              <Button
+                asChild
+                variant="link"
+                size="lg"
+                className="px-3 hover:no-underline">
+                <a href="/blog">Blog</a>
+              </Button>
+            </span>
+          </section>
           <hr className="my-4" />
-          <h4 className="mb-2 ml-2 mt-1 font-semibold">Projects</h4>
-          <span className='flex flex-col items-start [&_a]:px-4'>
-            <Button
-              asChild
-              variant="link"
-              size="lg"
-              className="hover:no-underline">
-              <a href="/designProject">Minimal Typography</a>
-            </Button>
-            <Button
-              asChild
-              variant="link"
-              size="lg"
-              className="hover:no-underline">
-              <a href="/flaskSite">Old Flask Website</a>
-            </Button>
-            <Button
-              asChild
-              variant="link"
-              size="lg"
-              className="hover:no-underline">
-              <a href="/gpt">GPT Chat</a>
-            </Button>
-            <Button
-              asChild
-              variant="link"
-              size="lg"
-              className="hover:no-underline">
-              <a href="/react">React + shadcn/ui</a>
-            </Button>
-          </span>
+          <section>
+            <h4 className="mb-2 ml-2 mt-1 font-semibold">Projects</h4>
+            <span className="flex flex-col items-start [&_a]:px-4">
+              <Button
+                asChild
+                variant="link"
+                size="lg"
+                className="hover:no-underline">
+                <a href="/designProject">Minimal Typography</a>
+              </Button>
+              <Button
+                asChild
+                variant="link"
+                size="lg"
+                className="hover:no-underline">
+                <a href="/flaskSite">Old Flask Website</a>
+              </Button>
+              <Button
+                asChild
+                variant="link"
+                size="lg"
+                className="hover:no-underline">
+                <a href="/gpt">GPT Chat</a>
+              </Button>
+              <Button
+                asChild
+                variant="link"
+                size="lg"
+                className="hover:no-underline">
+                <a href="/react">React + shadcn/ui</a>
+              </Button>
+            </span>
+          </section>
         </nav>
       </SheetContent>
     </Sheet>
