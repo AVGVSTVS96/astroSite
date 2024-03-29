f033ff35546f70f9d11f39ee4ed0ce179b1525d4
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-03-28 23:47:08 -0400

    wrap mainlinks button in <a> tag to fix link not working

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -12,7 +12,11 @@ export function SideMenu() {
   const mainLinks = [
     { name: 'Home', icon: <HomeIcon className="mr-2 size-4" />, href: '/' },
     { name: 'About', icon: <User className="mr-2 size-4" />, href: '/about' },
-    { name: 'Blog', icon: <NotebookText className="mr-2 size-4" />, href: '/blog' },
+    {
+      name: 'Blog',
+      icon: <NotebookText className="mr-2 size-4" />,
+      href: '/blog',
+    },
   ];
 
   const projectLinks = [
@@ -31,30 +35,32 @@ export function SideMenu() {
       </SheetTrigger>
       <SheetContent side="left" className="w-64">
         <SheetTitle className="font-bold">bassim</SheetTitle>
-        <nav className="mt-4 flex flex-col *:justify-start [&_a]:my-1.5">
+        <nav className="mt-4 flex flex-col">
           <section>
             {mainLinks.map((link) => (
               <span className="flex items-center" key={link.name}>
-                <Button
-                  variant="ghost"
-                  size="lg"
-                  className="h-9 w-full justify-start px-3 hover:no-underline">
-                  {link.icon}
-                  <a href={link.href}>{link.name}</a>
-                </Button>
+                <a href={link.href} className='w-full'>
+                  <Button
+                    variant="ghost"
+                    size="lg"
+                    className="h-9 w-full justify-start px-3 hover:no-underline">
+                    {link.icon}
+                    {link.name}
+                  </Button>
+                </a>
               </span>
             ))}
           </section>
           <hr className="my-4" />
           <section>
             <h4 className="mb-2 ml-2 mt-1 font-semibold">Projects</h4>
-            <span className="flex flex-col items-start [&_a]:h-full [&_a]:px-4">
+            <span className="flex flex-col items-start [&_a]:my-1.5 [&_a]:h-full [&_a]:px-4">
               {projectLinks.map((project) => (
                 <Button
                   asChild
                   variant="link"
                   size="lg"
-                  className="w-full justify-start text-muted-foreground hover:text-foreground hover:no-underline"
+                  className="text-muted-foreground w-full justify-start hover:text-foreground hover:no-underline"
                   key={project.name}>
                   <a href={project.href}>{project.name}</a>
                 </Button>
