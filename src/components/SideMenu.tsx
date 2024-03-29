f88ea14788605aaadf39c8e21aaf97b3a66d1c18
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2024-03-28 23:49:32 -0400

    Create sidebar nav menu with shadcn sheet component (#102)
    * Add src import alias
    
    * update import alias so shadcn cli adds components in correct directory
    
    * Add shadcn sheet component
    
    * update tsconfig import alias
    
    improve path alias
    
    * Add blank sheet component and import in Header
    
    * add sheet title
    
    * Add buttons and links to SideMenu component
    
    * format
    
    * rm unused styles
    
    * Add icons to main nav items in SideMenu
    
    * Add divider between main nav links and projects
    
    * Wrap projects links in span with flex-col and px-4 on links
    
    * Wrap menu sections in sections tags
    
    * Update button height and margin-y
    
    * Change text + hover color of project links, add more space between links
    
    * Update main nav button variant to ghost, remove asChild prop, move icons inside buttons, set button `w-full` and `justify-start`
    
    * Use map functions for nav and project links
    
    * wrap mainlinks button in <a> tag to fix link not working

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,74 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from '@components/ui/sheet';
+import { Button } from '@components/ui/button';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { HomeIcon, User, NotebookText } from 'lucide-react';
+
+export function SideMenu() {
+  const mainLinks = [
+    { name: 'Home', icon: <HomeIcon className="mr-2 size-4" />, href: '/' },
+    { name: 'About', icon: <User className="mr-2 size-4" />, href: '/about' },
+    {
+      name: 'Blog',
+      icon: <NotebookText className="mr-2 size-4" />,
+      href: '/blog',
+    },
+  ];
+
+  const projectLinks = [
+    { name: 'Minimal Typography', href: '/designProject' },
+    { name: 'Old Flask Website', href: '/flaskSite' },
+    { name: 'GPT Chat', href: '/gpt' },
+    { name: 'React + shadcn/ui', href: '/react' },
+  ];
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon">
+          <HamburgerMenuIcon className="size-5" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="w-64">
+        <SheetTitle className="font-bold">bassim</SheetTitle>
+        <nav className="mt-4 flex flex-col">
+          <section>
+            {mainLinks.map((link) => (
+              <span className="flex items-center" key={link.name}>
+                <a href={link.href} className='w-full'>
+                  <Button
+                    variant="ghost"
+                    size="lg"
+                    className="h-9 w-full justify-start px-3 hover:no-underline">
+                    {link.icon}
+                    {link.name}
+                  </Button>
+                </a>
+              </span>
+            ))}
+          </section>
+          <hr className="my-4" />
+          <section>
+            <h4 className="mb-2 ml-2 mt-1 font-semibold">Projects</h4>
+            <span className="flex flex-col items-start [&_a]:my-1.5 [&_a]:h-full [&_a]:px-4">
+              {projectLinks.map((project) => (
+                <Button
+                  asChild
+                  variant="link"
+                  size="lg"
+                  className="text-muted-foreground w-full justify-start hover:text-foreground hover:no-underline"
+                  key={project.name}>
+                  <a href={project.href}>{project.name}</a>
+                </Button>
+              ))}
+            </span>
+          </section>
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+}
