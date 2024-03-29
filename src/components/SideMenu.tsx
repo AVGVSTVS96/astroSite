628edce225310bb371715bcdc849f2a710c6a335
AVGVSTVS96 <bassim101@gmail.com>
2024-03-28 20:18:15 -0400

    Update main nav button variant to ghost, remove asChild prop, move icons inside buttons, set button `w-full` and `justify-start`

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -22,35 +22,32 @@ export function SideMenu() {
       </SheetTrigger>
       <SheetContent side="left" className="w-64">
         <SheetTitle className="font-bold">bassim</SheetTitle>
-        <nav className="mt-4 flex flex-col *:justify-start [&_a]:h-full [&_a]:my-2">
+        <nav className="mt-4 flex flex-col *:justify-start [&_a]:my-1.5">
           <section>
-            <span className="ml-2 flex items-center">
-              <HomeIcon className="size-4" />
+            <span className="flex items-center">
               <Button
-                asChild
-                variant="link"
+                variant="ghost"
                 size="lg"
-                className="px-3 hover:no-underline">
+                className="h-9 w-full justify-start px-3 hover:no-underline">
+                <HomeIcon className="mr-2 size-4" />
                 <a href="/">Home</a>
               </Button>
             </span>
-            <span className="ml-2 flex items-center">
-              <User className="size-4" />
+            <span className="flex items-center">
               <Button
-                asChild
-                variant="link"
+                variant="ghost"
                 size="lg"
-                className="px-3 hover:no-underline">
+                className="h-9 w-full justify-start px-3 hover:no-underline">
+                <User className="mr-2 size-4" />
                 <a href="/about">About</a>
               </Button>
             </span>
-            <span className="ml-2 flex items-center">
-              <NotebookText className="size-4" />
+            <span className="flex items-center">
               <Button
-                asChild
-                variant="link"
+                variant="ghost"
                 size="lg"
-                className="px-3 hover:no-underline">
+                className="h-9 w-full justify-start px-3 hover:no-underline">
+                <NotebookText className="mr-2 size-4" />
                 <a href="/blog">Blog</a>
               </Button>
             </span>
@@ -58,33 +55,33 @@ export function SideMenu() {
           <hr className="my-4" />
           <section>
             <h4 className="mb-2 ml-2 mt-1 font-semibold">Projects</h4>
-            <span className="flex flex-col items-start [&_a]:px-4">
+            <span className="flex flex-col items-start [&_a]:h-full [&_a]:px-4">
               <Button
                 asChild
                 variant="link"
                 size="lg"
-                className="hover:no-underline text-muted-foreground hover:text-foreground">
+                className="w-full justify-start text-muted-foreground hover:text-foreground hover:no-underline">
                 <a href="/designProject">Minimal Typography</a>
               </Button>
               <Button
                 asChild
                 variant="link"
                 size="lg"
-                className="hover:no-underline text-muted-foreground hover:text-foreground">
+                className="w-full justify-start text-muted-foreground hover:text-foreground hover:no-underline">
                 <a href="/flaskSite">Old Flask Website</a>
               </Button>
               <Button
                 asChild
                 variant="link"
                 size="lg"
-                className="hover:no-underline text-muted-foreground hover:text-foreground">
+                className="w-full justify-start text-muted-foreground hover:text-foreground hover:no-underline">
                 <a href="/gpt">GPT Chat</a>
               </Button>
               <Button
                 asChild
                 variant="link"
                 size="lg"
-                className="hover:no-underline text-muted-foreground hover:text-foreground">
+                className="w-full justify-start text-muted-foreground hover:text-foreground hover:no-underline">
                 <a href="/react">React + shadcn/ui</a>
               </Button>
             </span>
