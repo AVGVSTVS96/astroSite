450b78a8ee55d440ef6c898059780979a2253304
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-04-07 20:58:54 -0400

    Create shadcn-ui dropdown menu component, import in `ReactHeader`

---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@components/ui/dropdown-menu';
+import { projectLinks } from './navLinks';
+
+export function Dropdown({ children }: { children: React.ReactNode }) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="link"
+          className="hidden h-9 px-0 text-muted-foreground hover:text-foreground/80 hover:no-underline focus-visible:ring-0 data-[state='open']:text-foreground/80 sm:block">
+          {children}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-24 min-w-fit">
+        {projectLinks.map((project) => (
+          <DropdownMenuItem
+            key={project.name}
+            className="text-muted-foreground">
+            <a href={project.href}>{project.name}</a>
+          </DropdownMenuItem>
+        ))}{' '}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
