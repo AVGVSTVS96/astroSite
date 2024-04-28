2c0f31a0b66a7459a30e02f7d5409dbad0351b97
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2024-04-28 03:17:16 -0400

    Extract Sun and Moon icons to their own component (#277)
    Makes updating the icons easier, and the code more readable.

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -2,6 +2,15 @@ import { Moon, Sun } from 'lucide-react';
 import { Button } from '@components/ui/button';
 import { useThemeToggle } from '@hooks/useThemeToggle';
 
+export const ThemeIcon = () => {
+  return (
+    <>
+      <Sun className='size-5 scale-100 dark:scale-0' />
+      <Moon className='absolute size-5 scale-0 dark:scale-100' />
+    </>
+  );
+};
+
 export function ModeToggle() {
   const { toggleTheme } = useThemeToggle();
 
@@ -11,8 +20,7 @@ export function ModeToggle() {
       size="icon"
       onClick={toggleTheme}
       className="focus-visible:bg-accent focus-visible:ring-transparent">
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <ThemeIcon />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
