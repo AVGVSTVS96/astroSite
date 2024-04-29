9c210d575fbec6ebef9293e21d16371d9fb5ca4d
AVGVSTVS96 <bassim101@gmail.com>
2024-04-29 14:40:30 -0400

    Add `CollectionEntry` type to `getSortedPosts`

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,6 +1,9 @@
 import { getCollection } from 'astro:content';
+import type { CollectionEntry } from 'astro:content';
 
-export const sortedBlogPosts = (await getCollection('posts')).sort(
+type posts = CollectionEntry<'posts'>;
+
+export const sortedBlogPosts: posts[] = (await getCollection('posts')).sort(
   (a, b) =>
     Date.parse(b.data.pubDate.toString()) -
     Date.parse(a.data.pubDate.toString())
