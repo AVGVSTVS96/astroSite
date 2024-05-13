93c3105fec405034f8846e191de1ba7a69076c57
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2024-05-13 00:33:28 -0400

    Update `ChatUI` select styling, add more model options (#343)
    * Decrease `Footer.astro` `margin-top` to `mt-8` to match header's `margin-bottom`
    - **Create array of model groups and options, OpenAI and OpenAI Legacy**
    * **Update text colors, add separator between model groups**
    - **major refactor: improve `ChatUI` styling and structure**
      - Add key to group.model `SelectItems`
      - Extract form to it's own `ChatInput` component and use in 
    `Chat()`
      - Extract model selection logic to `useModel` hook
      - Use shadcn textarea instead of input element for chat input form
      - Change button styling and position absolutely inside textarea
      - Set outline ring to `ring-accent/20`
    
    
    * **redesign: `ChatUI` component**
      - Make height full using `calc(100dvh-165px)` 
      - Use flex-col-reverse to add new messages to bottom of container instead of top. This not only looks more appropriate, it **enables autoscrolling functionality without any additional JS**
      - Make `Card` more rounded - `rounded-2xl`
      - Update chat card heading size
      - Make message text larger `1rem`

--- src/components/ChatUI.tsx
+++ src/components/ChatUI.tsx
@@ -3,18 +3,36 @@ import { PaperPlaneIcon } from '@radix-ui/react-icons';
 
 import { Card, CardContent, CardFooter, CardHeader } from '@components/ui/card';
 import { Button } from '@components/ui/button';
-import { Input } from '@components/ui/input';
 import {
   Select,
   SelectTrigger,
   SelectValue,
   SelectContent,
+  SelectSeparator,
   SelectGroup,
   SelectLabel,
   SelectItem,
 } from '@components/ui/select';
-
+import { Textarea } from '@/components/ui/textarea';
+import { useModel } from '@/hooks/useModel';
 import { useChat, type UseChatHelpers, type UseChatOptions } from 'ai/react';
+
+const modelGroups = [
+  {
+    label: 'OpenAI',
+    models: [
+      { label: 'GPT-3.5-Turbo', value: 'gpt-3.5-turbo' },
+      { label: 'GPT-4-Turbo', value: 'gpt-4-turbo' },
+    ],
+  },
+  {
+    label: 'OpenAI Legacy',
+    models: [
+      { label: 'GPT-3.5-Turbo-0613', value: 'gpt-3.5-turbo-0613' },
+      { label: 'GPT-4-0613', value: 'gpt-4-0613' },
+    ],
+  },
+];
 
 interface ModelSelectorProps {
   selectedModel: string;
@@ -31,31 +49,87 @@ const ModelSelector: React.FC<ModelSelectorProps> = ({
 
   return (
     <Select value={selectedModel} onValueChange={handleModelChange}>
-      <SelectTrigger className="w-[180px]">
+      <SelectTrigger className="w-[180px] focus:ring-ring/20">
         <SelectValue placeholder="Select a model" />
       </SelectTrigger>
       <SelectContent>
-        <SelectGroup>
-          <SelectLabel className="text-xs">OpenAI</SelectLabel>
-          <SelectItem value="gpt-3.5-turbo">GPT-3.5</SelectItem>
-          <SelectItem value="gpt-4">GPT-4-Turbo</SelectItem>
-        </SelectGroup>
+        {modelGroups.map((group, index) => (
+          <SelectGroup key={index}>
+            <SelectLabel className="text-sm font-bold tracking-wide text-foreground">
+              {group.label}
+            </SelectLabel>
+            {group.models.map((item) => (
+              <SelectItem
+                key={item.value}
+                className="text-muted-foreground data-[state=checked]:text-foreground"
+                value={item.value}>
+                {item.label}
+              </SelectItem>
+            ))}
+            {index < modelGroups.length - 1 && <SelectSeparator />}
+          </SelectGroup>
+        ))}
       </SelectContent>
     </Select>
   );
 };
 
-export function Chat() {
-  const defaultModel: string = 'gpt-3.5-turbo';
-  const localStorageAvailable =
-    typeof window !== 'undefined' && window.localStorage;
-  
-  const model = localStorageAvailable
-    ? localStorage.getItem('selectedModel') || defaultModel
-    : defaultModel;
+interface ChatInputProps {
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  textareaRef: React.RefObject<HTMLTextAreaElement>;
+  input: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
 
-  const [selectedModel, setSelectedModel] =
-    React.useState<string>(model);
+const ChatInput: React.FC<ChatInputProps> = ({
+  handleSubmit,
+  textareaRef,
+  input,
+  handleInputChange,
+}) => {
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      handleSubmit(event);
+    }
+  };
+
+  React.useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [input]);
+
+  return (
+    <form onSubmit={handleSubmit} className="relative flex w-full items-center">
+      <Textarea
+        ref={textareaRef}
+        id="input"
+        name="prompt"
+        rows={1}
+        placeholder="Type your message..."
+        className="max-h-28 flex-1 resize-none rounded-xl py-4 pr-10 text-[1rem] focus-visible:ring-ring/20"
+        autoComplete="off"
+        value={input}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <Button
+        type="submit"
+        size="icon"
+        variant="secondary"
+        className="absolute bottom-3 right-3">
+        <PaperPlaneIcon className="size-[17px]" />
+        <span className="sr-only">Send</span>
+      </Button>
+    </form>
+  );
+};
+
+export function Chat() {
+  const defaultModel = 'gpt-3.5-turbo';
+  const { selectedModel, handleModelChange } = useModel(defaultModel);
 
   const chatOptions: UseChatOptions = {
     api: '/api/chatRoute',
@@ -64,42 +138,27 @@ export function Chat() {
   const { messages, input, handleInputChange, handleSubmit }: UseChatHelpers =
     useChat(chatOptions);
 
-  const handleModelChange = (model: string) => {
-    setSelectedModel(model);
-    localStorageAvailable ? localStorage.setItem('selectedModel', model) : null;
-  };
-
-  React.useEffect(() => {
-    const sendModelName = async () => {
-      await fetch('/api/chatRoute', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ modelName: selectedModel }),
-      });
-    };
-
-    sendModelName();
-  }, [selectedModel]);
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
 
   return (
-    <Card className="flex h-[clamp(300px,70vh,700px)] w-[clamp(260px,60vw,700px)] flex-col">
+    <Card className="flex h-[calc(100dvh-165px)] w-[clamp(260px,60vw,1000px)] flex-col rounded-2xl">
       <CardHeader className="h-18 flex flex-row items-center py-3">
         <div className="flex-1">
-          <p className="font-bold leading-none tracking-tight">ChatGPT</p>
+          <p className="text-lg font-bold leading-none tracking-tight">
+            ChatGPT
+          </p>
         </div>
         <ModelSelector
           selectedModel={selectedModel}
           onModelChange={handleModelChange}
         />
       </CardHeader>
-      <CardContent className="grow overflow-y-auto">
+      <CardContent className="flex grow flex-col-reverse overflow-y-auto">
         <div className="space-y-4">
           {messages.map((message) => (
             <div
               key={message.id}
-              className={`flex w-max max-w-[75%] whitespace-pre-wrap flex-col gap-2 rounded-lg px-3 py-2 text-sm ${
+              className={`flex w-max max-w-[75%] flex-col gap-2 whitespace-pre-wrap rounded-lg px-3 py-2 ${
                 message.role === 'user'
                   ? 'ml-auto bg-primary text-primary-foreground'
                   : 'bg-muted'
@@ -109,24 +168,13 @@ export function Chat() {
           ))}
         </div>
       </CardContent>
-      <CardFooter className="mt-6">
-        <form
-          onSubmit={handleSubmit}
-          className="flex w-full items-center space-x-2">
-          <Input
-            id="input"
-            name="prompt"
-            placeholder="Type your message..."
-            className="flex-1"
-            autoComplete="off"
-            value={input}
-            onChange={handleInputChange}
-          />
-          <Button type="submit" size="icon">
-            <PaperPlaneIcon className="h-4 w-4" />
-            <span className="sr-only">Send</span>
-          </Button>
-        </form>
+      <CardFooter className="mt-6 ">
+        <ChatInput
+          handleSubmit={handleSubmit}
+          textareaRef={textareaRef}
+          input={input}
+          handleInputChange={handleInputChange}
+        />
       </CardFooter>
     </Card>
   );
---
+++ src/hooks/useModel.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+export const useModel = (defaultModel: string) => {
+  const localStorageAvailable = typeof window !== 'undefined' && window.localStorage;
+  const [selectedModel, setSelectedModel] = useState<string>(() => {
+    if (localStorageAvailable) {
+      return localStorage.getItem('selectedModel') || defaultModel;
+    }
+
+    return defaultModel;
+  });
+
+  const handleModelChange = (model: string) => {
+    setSelectedModel(model);
+
+    if (localStorageAvailable) {
+      localStorage.setItem('selectedModel', model);
+    }
+  };
+
+  useEffect(() => {
+    const sendModelName = async () => {
+      await fetch('/api/chatRoute', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          modelName: selectedModel
+        })
+      });
+    };
+
+    sendModelName();
+  }, [selectedModel]);
+  return {
+    selectedModel,
+    handleModelChange
+  };
+};
+  
