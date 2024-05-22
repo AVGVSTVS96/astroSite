948281ccaf4bbbd0d7d554890c086527d46ccc2b
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2024-05-22 06:50:53 -0400

    feat: Use new `isActiveLink` utility function to highlight active page link (#369)
    - feat: Add isActiveLink utility function
      - The commit adds a new utility function `isActiveLink` to the `src/utils/isActiveLink.ts` file. This function is used to determine if a link is active based on the current path and the link's href. The function returns Tailwind CSS class names based on the result, which are then used to highlight the active links
    
    * feat: Update `ReactHeader` component to use `isActiveLink` utility
      - Use the newly added `isActiveLink` utility to highlight page links if the page is active.
    
    - feat: Update `ProjectsDropdown` component to use `isActiveLink` utility
      - Use the newly added `isActiveLink` utility to highlight page links if the page is active.

--- src/components/ProjectsDropdown.tsx
+++ src/components/ProjectsDropdown.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { DropdownMenu, DropdownMenuItem } from './DropdownMenu';
 import { ChevronDown } from 'lucide-react';
 import { projectLinks } from './navLinks';
+import { isActiveLink } from '@utils/isActiveLink';
 
 export const ProjectsDropdown: React.FC = () => {
+  const currentPath =
+    typeof window !== 'undefined' ? window.location.pathname : '';
+
   return (
     <DropdownMenu variant="link" ariaLabel="Open projects dropdown menu">
       <span className="flex items-center">
@@ -13,16 +17,19 @@ export const ProjectsDropdown: React.FC = () => {
           className="ml-1.5 mt-[1.5px] transition-all group-data-[state=open]:rotate-180"
         />
       </span>
-      {projectLinks.map((link) => (
-        <DropdownMenuItem
-          key={link.href}
-          className="py-2 text-muted-foreground"
-          onSelect={() => {
-            window.location.href = link.href;
-          }}>
-          {link.name}
-        </DropdownMenuItem>
-      ))}
+      {projectLinks.map((link) => {
+        const activeClass = isActiveLink(currentPath, link.href);
+        return (
+          <DropdownMenuItem
+            key={link.href}
+            className={`py-2 ${activeClass}`}
+            onSelect={() => {
+              window.location.href = link.href;
+            }}>
+            {link.name}
+          </DropdownMenuItem>
+        );
+      })}
     </DropdownMenu>
   );
 };
