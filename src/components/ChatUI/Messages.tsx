2abe09abf26e2eca5f6627c23b600a93a70b0dc6
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-05-31 14:29:37 +0530

    Refactor `Messages.tsx` - Separate `SyntaxHighlighter` to it's own component - Set theme to `nightOwl` - Organize message styles using `cn()` and styles variables - Update `CodeComponentProps` interface, remove optional(`?`) from required props

--- src/components/ChatUI/Messages.tsx
+++ src/components/ChatUI/Messages.tsx
@@ -2,49 +2,62 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { type UseChatHelpers } from 'ai/react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { cn } from '@/lib/utils';
 
 interface MessagesProps {
   messages: UseChatHelpers['messages'];
 }
 
 interface CodeComponentProps {
-  inline?: boolean;
-  className?: string;
-  children?: React.ReactNode;
+  inline: boolean;
+  className: string;
+  children: React.ReactNode;
 }
 
+const CodeHighlight = ({
+  inline,
+  className,
+  children,
+  ...props
+}: CodeComponentProps) => {
+  const match = /language-(\w+)/.exec(className || '');
+  const language = match ? match[1] : 'plaintext';
+
+  return !inline ? (
+    <SyntaxHighlighter
+      language={language}
+      style={nightOwl}
+      PreTag="div"
+      {...props}>
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
+  );
+};
+
 export const Messages: React.FC<MessagesProps> = ({ messages }) => {
+  const baseMessageStyles =
+    'prose prose-slate prose-invert flex w-max max-w-[75%] flex-col rounded-lg px-3 py-2 prose-pre:bg-transparent prose-pre:p-0 prose-ul:mt-0';
+  const userMessageStyles =
+    'ml-auto bg-primary text-primary-foreground prose-code:text-primary-foreground';
+  const assistantMessageStyles = 'bg-muted text-foreground';
+
   return (
     <div className="space-y-4">
       {messages.map((message) => (
         <div
           key={message.id}
-          className={`prose prose-slate prose-invert flex w-max max-w-[75%] flex-col rounded-lg px-3 py-2 prose-ul:mt-0  ${message.role === 'user' ? 'ml-auto bg-primary text-primary-foreground' : 'bg-muted text-foreground'}`}>
+          className={cn(
+            baseMessageStyles,
+            message.role === 'user' ? userMessageStyles : assistantMessageStyles
+          )}>
           <ReactMarkdown
             components={{
-              code({
-                inline,
-                className,
-                children,
-                ...props
-              }: CodeComponentProps) {
-                const match = /language-(\w+)/.exec(className || '');
-                const language = match ? match[1] : 'plaintext';
-                return !inline ? (
-                  <SyntaxHighlighter
-                    language={language}
-                    style={atomDark}
-                    PreTag="div"
-                    {...props}>
-                    {String(children).replace(/\n$/, '')}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              },
+              code: CodeHighlight,
             }}>
             {message.content}
           </ReactMarkdown>
