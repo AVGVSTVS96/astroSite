b328a2fd25c2e9ea85074a5566fb0819851961a2
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-06-29 17:30:31 -0400

    Create `ShikiHihlighter` component and use it in `CodeHighlight`
    `ShikiHihlighter` uses the `useShiki` hook to highlight code and provides a component to easily use syntax highlighting
    
    The `ShikiHihlighter` component is then used in `CodeHighlight`, where `isInline` is used to determine what to render.

--- src/components/ChatUI/CodeHighlight.tsx
+++ src/components/ChatUI/CodeHighlight.tsx
@@ -2,8 +2,7 @@ import type { ReactNode } from 'react';
 import type { BundledLanguage } from 'shiki';
 import type { Element } from 'hast';
 import { isInlineCode } from '@utils/isInlineCode';
-import { useShikiHighlighter } from '@hooks/useShiki';
-
+import { ShikiHighlighter } from './ShikiHihlighter';
 
 interface CodeHighlightProps {
   className: string;
@@ -17,29 +16,18 @@ export const CodeHighlight = ({
   node,
   ...props
 }: CodeHighlightProps) => {
-  const theme = 'houston';
-  const code = String(children);
   const match = /language-(\w+)/.exec(className || '');
   const language = match ? (match[1] as BundledLanguage) : undefined;
 
   const isInline: boolean = isInlineCode(node);
 
-  const highlightedCode: ReactNode | null = useShikiHighlighter(
-    code,
-    language,
-    theme,
-  );
-
   return !isInline ? (
-    <div
-      className={`shiki not-prose relative [&_pre]:overflow-auto [&_pre]:px-6 [&_pre]:py-5 ${className || ''}`}>
-      {language ? (
-        <span className="absolute right-3 top-2 text-xs tracking-tighter text-muted-foreground/85">
-          {language}
-        </span>
-      ) : null}
-      {highlightedCode}
-    </div>
+    <ShikiHighlighter
+      language={language as BundledLanguage}
+      theme={'houston'}
+      {...props}>
+      {String(children)}
+    </ShikiHighlighter>
   ) : (
     <code className={className} {...props}>
       {children}
