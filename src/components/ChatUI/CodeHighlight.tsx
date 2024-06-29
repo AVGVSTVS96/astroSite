f85bda70cdb5e80ca266ffc8c4bdadaa474434c0
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-06-29 00:06:37 -0400

    Create `useShiki` hook, use in `CodeHighlight` component
    - Create `useShiki` hook to handle code highlighting logic
    - Call `useShiki` hook in `CodeHighlight` component
    - Remove unneeded props and imports from `CodeHighlight` component

--- src/components/ChatUI/CodeHighlight.tsx
+++ src/components/ChatUI/CodeHighlight.tsx
@@ -1,14 +1,9 @@
-import { useState, useEffect, type ReactNode } from 'react';
-import {
-  codeToHtml,
-  ShikiError,
-  type BundledLanguage,
-  type BundledTheme,
-} from 'shiki';
-import parse from 'html-react-parser';
+import type { ReactNode } from 'react';
+import type { BundledLanguage } from 'shiki';
 import type { Element } from 'hast';
 import { isInlineCode } from '@/lib/utils';
-import { removeTabIndexFromPre } from '@/lib/utils/shikiTransformers';
+import { useShikiHighlighter } from '@hooks/useShiki';
+
 
 interface CodeHighlightProps {
   className?: string | undefined;
@@ -22,47 +17,18 @@ export const CodeHighlight = ({
   node,
   ...props
 }: CodeHighlightProps): JSX.Element => {
-  const [highlightedCode, setHighlightedCode] = useState<ReactNode | null>(
-    null
-  );
-  const theme: BundledTheme = 'catppuccin-mocha';
+  const theme = 'houston';
   const code = String(children);
   const match = className?.match(/language-(\w+)/);
-  const language = match ? match[1] : undefined;
+  const language = match ? (match[1] as BundledLanguage) : undefined;
 
   const isInline: boolean | undefined = node ? isInlineCode(node) : undefined;
 
-  useEffect(() => {
-    if (!isInline) {
-      const highlightCode = async () => {
-        try {
-          const html = await codeToHtml(code, {
-            lang: language as BundledLanguage,
-            theme,
-            transformers: [removeTabIndexFromPre],
-          });
-          setHighlightedCode(parse(html));
-        } catch (error) {
-          if (
-            error instanceof ShikiError &&
-            error.message.includes('Language')
-          ) {
-            const fallbackHtml = await codeToHtml(code, {
-              lang: 'plaintext',
-              theme,
-              transformers: [removeTabIndexFromPre],
-            });
-            setHighlightedCode(parse(fallbackHtml));
-          } else {
-            console.error('Unexpected Shiki error:', error);
-            throw error;
-          }
-        }
-      };
-
-      highlightCode();
-    }
-  }, [code]);
+  const highlightedCode: ReactNode | null = useShikiHighlighter(
+    code,
+    language,
+    theme,
+  );
 
   return !isInline ? (
     <div className="shiki not-prose relative [&_pre]:overflow-auto [&_pre]:rounded-lg [&_pre]:px-6 [&_pre]:py-5">
