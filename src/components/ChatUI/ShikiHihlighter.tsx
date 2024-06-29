7a0e8c3db33f016265a4519c2742fd6b8c47288e
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-06-29 15:43:45 -0400

    update `preTag` prop capitalization

---
+++ src/components/ChatUI/ShikiHihlighter.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { type BundledTheme, type BundledLanguage, codeToHtml } from 'shiki';
+import parse from 'html-react-parser';
+
+interface ShikiHighlighterProps {
+  language: BundledLanguage;
+  children: string;
+  theme: BundledTheme;
+  preTag: keyof JSX.IntrinsicElements;
+}
+
+export const ShikiHighlighter: React.FC<ShikiHighlighterProps> = ({
+  language,
+  children,
+  theme,
+  preTag = 'pre',
+}) => {
+  const [highlightedCode, setHighlightedCode] =
+    useState<React.ReactNode | null>(null);
+
+  useEffect(() => {
+    if (language) {
+      codeToHtml(children, { lang: language, theme }).then((html) => {
+        setHighlightedCode(parse(html));
+      });
+    }
+  }, [children, language, theme]);
+
+  if (!highlightedCode) {
+    return React.createElement(
+      preTag,
+      null,
+      React.createElement('code', null, children)
+    );
+  }
+
+  return React.createElement(preTag, {
+    className: 'shiki not-prose',
+    dangerouslySetInnerHTML: { __html: highlightedCode },
+  });
+};
