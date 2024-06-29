38f205b29b43e91c7867595d087f381c233a87d3
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-06-29 17:30:31 -0400

    Create `ShikiHihlighter` component and use it in `CodeHighlight`
    `ShikiHihlighter` uses the `useShiki` hook to highlight code and provides a component to easily use syntax highlighting
    
    The `ShikiHihlighter` component is then used in `CodeHighlight`, where `isInline` is used to determine what to render.

---
+++ src/components/ChatUI/ShikiHihlighter.tsx
@@ -0,0 +1,34 @@
+import type React from 'react';
+import { useShikiHighlighter } from '@/hooks/useShiki';
+import type { BundledTheme, BundledLanguage } from 'shiki';
+
+interface ShikiHighlighterProps {
+  language: BundledLanguage;
+  children: string;
+  theme: BundledTheme;
+  PreTag?: keyof JSX.IntrinsicElements;
+}
+
+export const ShikiHighlighter: React.FC<ShikiHighlighterProps> = ({
+  language,
+  children,
+  theme,
+  PreTag = 'pre',
+}) => {
+  const highlightedCode = useShikiHighlighter(
+    children,
+    language,
+    theme,
+  );
+
+  return (
+    <PreTag className="shiki not-prose relative [&_pre]:overflow-auto [&_pre]:rounded-lg [&_pre]:px-6 [&_pre]:py-5">
+      {language ? (
+        <span className="absolute right-3 top-2 text-xs tracking-tighter text-muted-foreground/85">
+          {language}
+        </span>
+      ) : null}
+      {highlightedCode}
+    </PreTag>
+  );
+};
