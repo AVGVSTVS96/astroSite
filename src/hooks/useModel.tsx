36e224b8141296c221e53d63970b69782ffa743f
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2024-07-05 23:50:22 -0400

    refactor(accentColorS.., useModel): Remove React.FC type and rename consts (#482)
    Rename `localStorageAccessible` to `isBrowser`, make small updates to logic in both `AccentColorSelector.tsx` and `useModel.tsx`

--- src/hooks/useModel.tsx
+++ src/hooks/useModel.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 
 export const useModel = (defaultModel: string) => {
-  const localStorageAvailable = typeof window !== 'undefined' && window.localStorage;
+  const isBrowser = typeof window !== 'undefined' && window.localStorage;
   const [selectedModel, setSelectedModel] = useState<string>(() => {
-    if (localStorageAvailable) {
+    if (isBrowser) {
       return localStorage.getItem('selectedModel') || defaultModel;
     }
 
@@ -12,10 +12,7 @@ export const useModel = (defaultModel: string) => {
 
   const handleModelChange = (model: string) => {
     setSelectedModel(model);
-
-    if (localStorageAvailable) {
-      localStorage.setItem('selectedModel', model);
-    }
+    isBrowser && localStorage.setItem('selectedModel', model);
   };
 
   useEffect(() => {
@@ -23,11 +20,11 @@ export const useModel = (defaultModel: string) => {
       await fetch('/api/chat', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          modelName: selectedModel
-        })
+          modelName: selectedModel,
+        }),
       });
     };
 
@@ -35,7 +32,6 @@ export const useModel = (defaultModel: string) => {
   }, [selectedModel]);
   return {
     selectedModel,
-    handleModelChange
+    handleModelChange,
   };
 };
-  
