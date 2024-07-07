869536c3b49e08bcc593804a47ef27defd3f1520
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-07-07 13:01:14 -0400

    refactor: Remove usunsed imports

--- src/components/AccentColorSelector.tsx
+++ src/components/AccentColorSelector.tsx
@@ -1,4 +1,3 @@
-import type React from 'react';
 import { useState, useEffect } from 'react';
 import { DropdownMenu, DropdownMenuItem } from './DropdownMenu';
 import { Arrow } from '@radix-ui/react-tooltip';
