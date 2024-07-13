9e04337944274bb13fba9710c9713fb5d32334b3
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-07-13 10:34:48 -0400

    feat(ChatUI):  Add language to code blocks, add inline code check, and refactor
    - Separate CodeHighlight to it's own file
    - Add language label to code blocks
    - Add `isInline` util to check if a code block is inline

--- src/components/ChatUI/Messages.tsx
+++ src/components/ChatUI/Messages.tsx
@@ -1,62 +1,23 @@
-import type React from 'react';
 import ReactMarkdown from 'react-markdown';
-import type { UseChatHelpers } from '@ai-sdk/react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import rainglowAzureContrast from '@/styles/rainglowAzureContrast';
 import { cn } from '@/lib/utils';
+import { CodeHighlight } from './CodeHighlight';
+import type { Message } from '@ai-sdk/react';
 
-interface MessagesProps {
-  messages: UseChatHelpers['messages'];
-}
+const baseMessageStyles: string =
+  'prose prose-slate flex w-max max-w-[75%] flex-col rounded-lg px-3 py-2 prose-p:my-0 prose-pre:my-2 prose-pre:bg-transparent prose-pre:p-0 prose-ul:mt-0';
 
-interface CodeComponentProps {
-  inline: boolean;
-  className: string;
-  children: React.ReactNode;
-}
-
-const CodeHighlight = ({
-  inline,
-  className,
-  children,
-  ...props
-}: CodeComponentProps) => {
-  const match = /language-(\w+)/.exec(className || '');
-  const language = match ? match[1] : 'plaintext';
-
-  return !inline && language !== 'plaintext' ? (
-    <SyntaxHighlighter
-      language={language}
-      style={
-        rainglowAzureContrast as {
-          [key: string]: React.CSSProperties;
-        }
-      }
-      PreTag="div"
-      {...props}>
-      {String(children).replace(/\n$/, '')}
-    </SyntaxHighlighter>
-  ) : (
-    <code className={className} {...props}>
-      {children}
-    </code>
-  );
+const messageStyles: Record<string, Array<string>> = {
+  user: [
+    baseMessageStyles,
+    'ml-auto bg-primary text-primary-foreground prose-headings:text-primary-foreground prose-code:text-primary-foreground prose-blockquote:text-primary-foreground prose-a:text-primary-foreground prose-strong:text-primary-foreground',
+  ],
+  assistant: [
+    baseMessageStyles,
+    'bg-muted dark:prose-invert prose-code:text-foreground',
+  ],
 };
 
-export const Messages: React.FC<MessagesProps> = ({ messages }) => {
-  const baseMessageStyles: string =
-    'prose prose-slate flex w-max max-w-[75%] flex-col rounded-lg px-3 py-2 prose-p:my-0 prose-pre:my-2 prose-pre:bg-transparent prose-pre:p-0 prose-ul:mt-0';
-  const messageStyles: Record<string, string | Array<string>> = {
-    user: [
-      baseMessageStyles,
-      'ml-auto bg-primary text-primary-foreground prose-headings:text-primary-foreground prose-code:text-primary-foreground prose-blockquote:text-primary-foreground prose-a:text-primary-foreground prose-strong:text-primary-foreground',
-    ],
-    assistant: [
-      baseMessageStyles,
-      'bg-muted dark:prose-invert prose-code:text-foreground',
-    ],
-  };
-
+export const Messages = ({ messages }: { messages: Message[] }) => {
   return (
     <div className="space-y-4">
       {messages.map((message) => (
