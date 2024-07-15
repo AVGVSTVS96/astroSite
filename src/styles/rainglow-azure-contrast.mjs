165af9991271a166648fd981f92db0bf1c92cfd2
AVGVSTVS96 <bassim101@gmail.com>
2024-07-15 17:49:48 -0400

    chore(shiki): Use Shiki to create highlighter with custom theme
    Create a hook to highlight code using custom theme and a highlighter created with that custom theme

---
+++ src/styles/rainglow-azure-contrast.mjs
@@ -0,0 +1,508 @@
+const rainglowAzureContrast = Object.freeze({
+  colors: {
+    'activityBar.background': '#080a0d',
+    'activityBar.foreground': '#ffffff',
+    'activityBarBadge.background': '#6ab0a3',
+    'activityBarBadge.foreground': '#ffffff',
+    'badge.background': '#6ab0a3',
+    'badge.foreground': '#ffffff',
+    'button.background': '#52708b',
+    'button.foreground': '#ffffff',
+    'dropdown.background': '#000000',
+    'dropdown.border': '#000000',
+    'dropdown.foreground': '#ffffff',
+    'editor.background': '#040507',
+    'editor.foreground': '#ffffff',
+    'editor.hoverHighlightBackground': '#6ab0a322',
+    'editor.lineHighlightBackground': '#0a0c11',
+    'editor.selectionBackground': '#47959a55',
+    'editor.wordHighlightBackground': '#508aaa22',
+    'editor.wordHighlightStrongBackground': '#52708b22',
+    'editorBracketMatch.background': '#6ab0a344',
+    'editorBracketMatch.border': '#52708b',
+    'editorCursor.foreground': '#f8f8f0',
+    'editorGroupHeader.tabsBackground': '#0a0c11',
+    'editorGutter.addedBackground': '#a7da1e',
+    'editorGutter.background': '#000000',
+    'editorGutter.deletedBackground': '#e61f44',
+    'editorGutter.modifiedBackground': '#f7b83d',
+    'editorIndentGuide.background': '#171c27',
+    'editorLineNumber.foreground': '#293348',
+    'editorLink.activeForeground': '#6ab0a3',
+    'editorOverviewRuler.addedForeground': '#a7da1e',
+    'editorOverviewRuler.deletedForeground': '#e61f44',
+    'editorOverviewRuler.errorForeground': '#e61f44',
+    'editorOverviewRuler.findMatchForeground': '#52708b55',
+    'editorOverviewRuler.infoForeground': '#9d37fc',
+    'editorOverviewRuler.modifiedForeground': '#f7b83d',
+    'editorOverviewRuler.warningForeground': '#f7b83d',
+    'editorRuler.foreground': '#171c27',
+    'editorSuggestWidget.foreground': '#ffffff',
+    'editorSuggestWidget.highlightForeground': '#6ab0a3',
+    'editorSuggestWidget.selectedBackground': '#293348',
+    'editorWhitespace.foreground': '#171c27',
+    'editorWidget.background': '#171c27',
+    'editorWidget.border': '#293348',
+    'extensionButton.prominentBackground': '#6ab0a3',
+    'extensionButton.prominentForeground': '#ffffff',
+    'extensionButton.prominentHoverBackground': '#8bc2b8',
+    focusBorder: '#6ab0a3',
+    foreground: '#ffffff',
+    'gitDecoration.conflictingResourceForeground': '#9d37fc',
+    'gitDecoration.deletedResourceForeground': '#e61f44',
+    'gitDecoration.ignoredResourceForeground': '#3c4b68',
+    'gitDecoration.modifiedResourceForeground': '#f7b83d',
+    'gitDecoration.untrackedResourceForeground': '#a7da1e',
+    'input.background': '#000000',
+    'input.border': '#202838',
+    'input.foreground': '#ffffff',
+    'input.placeholderForeground': '#252f41',
+    'inputOption.activeBorder': '#52708b',
+    'list.activeSelectionBackground': '#52708b',
+    'list.activeSelectionForeground': '#040507',
+    'list.focusBackground': '#202838',
+    'list.hoverBackground': '#0a0c11',
+    'list.inactiveSelectionBackground': '#040507',
+    'panel.background': '#171c27',
+    'panel.border': '#293348',
+    'panelTitle.activeBorder': '#6ab0a3',
+    'panelTitle.inactiveForeground': '#455679',
+    'peekView.border': '#6ab0a3',
+    'peekViewEditor.background': '#000000',
+    'peekViewEditor.matchHighlightBackground': '#52708b55',
+    'peekViewEditorGutter.background': '#000000',
+    'peekViewResult.background': '#0d1117',
+    'peekViewResult.fileForeground': '#f5f5f5',
+    'peekViewResult.lineForeground': '#e6e6e6',
+    'peekViewResult.matchHighlightBackground': '#52708b55',
+    'peekViewResult.selectionBackground': '#080a0d',
+    'peekViewResult.selectionForeground': '#ffffff',
+    'peekViewTitle.background': '#000000',
+    'peekViewTitleDescription.foreground': '#f7f7f7',
+    'peekViewTitleLabel.foreground': '#ffffff',
+    'progressBar.background': '#508aaa',
+    'scrollbar.shadow': '#000000',
+    'scrollbarSlider.activeBackground': '#6ab0a3aa',
+    'scrollbarSlider.background': '#293348aa',
+    'scrollbarSlider.hoverBackground': '#6ab0a355',
+    'sideBar.background': '#0d1117',
+    'sideBar.foreground': '#ffffff',
+    'sideBarSectionHeader.background': '#131821',
+    'sideBarSectionHeader.foreground': '#ffffff',
+    'statusBar.background': '#6ab0a3',
+    'statusBar.foreground': '#ffffff',
+    'statusBar.noFolderBackground': '#52708b',
+    'tab.inactiveBackground': '#0b0e14',
+    'tab.inactiveForeground': '#4e6289',
+    'terminal.ansiBlack': '#0d1117',
+    'terminal.ansiBlue': '#508aaa',
+    'terminal.ansiBrightBlack': '#293348',
+    'terminal.ansiBrightBlue': '#94b8cc',
+    'terminal.ansiBrightCyan': '#add3cc',
+    'terminal.ansiBrightGreen': '#add3cc',
+    'terminal.ansiBrightMagenta': '#94b8cc',
+    'terminal.ansiBrightRed': '#f03e5f',
+    'terminal.ansiBrightWhite': '#ffffff',
+    'terminal.ansiBrightYellow': '#89a3ba',
+    'terminal.ansiCyan': '#6ab0a3',
+    'terminal.ansiGreen': '#6ab0a3',
+    'terminal.ansiMagenta': '#508aaa',
+    'terminal.ansiRed': '#ba0e2e',
+    'terminal.ansiWhite': '#ffffff',
+    'terminal.ansiYellow': '#52708b',
+    'terminal.background': '#000000',
+    'terminal.foreground': '#ffffff',
+    'titleBar.activeBackground': '#000000',
+  },
+  displayName: 'Rainglow Azure Contrast',
+  name: 'rainglow-azure-contrast',
+  tokenColors: [
+    {
+      scope: 'comment',
+      settings: {
+        foreground: '#414D62',
+      },
+    },
+    {
+      scope: 'string',
+      settings: {
+        foreground: '#64AEB3',
+      },
+    },
+    {
+      scope: 'constant.numeric',
+      settings: {
+        foreground: '#64AEB3',
+      },
+    },
+    {
+      scope: 'constant.language',
+      settings: {
+        foreground: '#52708B',
+      },
+    },
+    {
+      scope: ['constant.character', 'constant.other'],
+      settings: {
+        foreground: '#52708B',
+      },
+    },
+    {
+      scope: 'variable',
+      settings: {
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'keyword',
+      settings: {
+        foreground: '#508AAA',
+      },
+    },
+    {
+      scope: 'storage',
+      settings: {
+        foreground: '#52708B',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'storage.type',
+      settings: {
+        foreground: '#6AB0A3',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'entity.name.class',
+      settings: {
+        foreground: '#52708B',
+        fontStyle: 'underline',
+      },
+    },
+    {
+      scope: 'entity.other.inherited-class',
+      settings: {
+        foreground: '#52708B',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'entity.name.function',
+      settings: {
+        foreground: '#508AAA',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'variable.parameter',
+      settings: {
+        foreground: '#FFFFFF',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'entity.name.tag',
+      settings: {
+        foreground: '#6AB0A3',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'entity.other.attribute-name',
+      settings: {
+        foreground: '#52708B',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'support.function',
+      settings: {
+        foreground: '#6AB0A3',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'support.constant',
+      settings: {
+        foreground: '#52708B',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: ['support.type', 'support.class'],
+      settings: {
+        foreground: '#52708B',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'support.other.variable',
+      settings: {
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'invalid',
+      settings: {
+        foreground: '#CF433E',
+        background: '#664E4D',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'invalid.deprecated',
+      settings: {
+        foreground: '#CF433E',
+        background: '#664E4D',
+      },
+    },
+    {
+      scope: 'meta.structure.dictionary.json string.quoted.double.json',
+      settings: {
+        foreground: '#64AEB3',
+      },
+    },
+    {
+      scope: ['meta.diff', 'meta.diff.header'],
+      settings: {
+        foreground: '#414D62',
+      },
+    },
+    {
+      scope: 'markup.deleted',
+      settings: {
+        foreground: '#E61F44',
+      },
+    },
+    {
+      scope: 'markup.inserted',
+      settings: {
+        foreground: '#A6E22E',
+      },
+    },
+    {
+      scope: 'markup.changed',
+      settings: {
+        foreground: '#F7B83D',
+      },
+    },
+    {
+      scope: 'constant.numeric.line-number.find-in-files - match',
+      settings: {
+        foreground: '#8FBE00A0',
+      },
+    },
+    {
+      scope: 'entity.name.filename.find-in-files',
+      settings: {
+        foreground: '#E6DB74',
+      },
+    },
+    {
+      scope: 'keyword.other',
+      settings: {
+        foreground: '#607190',
+      },
+    },
+    {
+      scope: [
+        'meta.property-value',
+        'support.constant.property-value',
+        'constant.other.color',
+      ],
+      settings: {
+        foreground: '#64AEB3',
+      },
+    },
+    {
+      scope: 'meta.structure.dictionary.json string.quoted.double.json',
+      settings: {
+        foreground: '#52708B',
+      },
+    },
+    {
+      scope: 'meta.structure.dictionary.value.json string.quoted.double.json',
+      settings: {
+        foreground: '#64AEB3',
+      },
+    },
+    {
+      scope: 'meta.property-name support.type.property-name',
+      settings: {
+        fontStyle: 'normal',
+      },
+    },
+    {
+      scope: 'meta.property-value punctuation.separator.key-value',
+      settings: {
+        foreground: '#FFFFFF',
+      },
+    },
+    {
+      scope: [
+        'keyword.other.use',
+        'keyword.other.function.use',
+        'keyword.other.namespace',
+        'keyword.other.new',
+        'keyword.other.special-method',
+        'keyword.other.unit',
+        'keyword.other.use-as',
+      ],
+      settings: {
+        foreground: '#6AB0A3',
+      },
+    },
+    {
+      scope: [
+        'meta.use support.class.builtin',
+        'meta.other.inherited-class support.class.builtin',
+      ],
+      settings: {
+        foreground: '#6AB0A3',
+        fontStyle: 'normal',
+      },
+    },
+    {
+      scope: 'variable.other',
+      settings: {
+        foreground: '#FFFFFF',
+        fontStyle: 'normal',
+      },
+    },
+    {
+      scope: 'variable.parameter.function.coffee',
+      settings: {
+        foreground: '#64AEB3',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: 'entity.name.section.markdown',
+      settings: {
+        foreground: '#52708B',
+      },
+    },
+    {
+      scope: 'punctuation.definition.heading.markdown',
+      settings: {
+        foreground: '#508AAA',
+      },
+    },
+    {
+      scope: 'markup.raw.inline.markdown',
+      settings: {
+        foreground: '#64AEB3',
+      },
+    },
+    {
+      scope: [
+        'punctuation.definition.bold.markdown',
+        'punctuation.definition.italic.markdown',
+      ],
+      settings: {
+        foreground: '#508AAA',
+      },
+    },
+    {
+      scope: [
+        'punctuation.definition.string.begin.markdown',
+        'punctuation.definition.string.end.markdown',
+      ],
+      settings: {
+        foreground: '#508AAA',
+      },
+    },
+    {
+      scope: 'punctuation.definition.metadata.markdown',
+      settings: {
+        foreground: '#508AAA',
+      },
+    },
+    {
+      scope: [
+        'markup.underline.link.markdown',
+        'markup.underline.link.image.markdown',
+        'meta.image.inline.markdown',
+      ],
+      settings: {
+        foreground: '#6AB0A3',
+        fontStyle: '',
+      },
+    },
+    {
+      scope: ['markup.bold.markdown', 'markup.italic.markdown'],
+      settings: {
+        foreground: '#6AB0A3',
+      },
+    },
+    {
+      scope: 'markup.italic.markdown',
+      settings: {
+        fontStyle: 'italic',
+      },
+    },
+    {
+      scope: 'markup.bold.markdown',
+      settings: {
+        fontStyle: 'bold',
+      },
+    },
+    {
+      scope: 'markup.raw.block.markdown',
+      settings: {
+        foreground: '#664E4D',
+      },
+    },
+    {
+      scope: 'markup.deleted.git_gutter',
+      settings: {
+        foreground: '#E61F44',
+      },
+    },
+    {
+      scope: 'markup.inserted.git_gutter',
+      settings: {
+        foreground: '#A7DA1E',
+      },
+    },
+    {
+      scope: 'markup.changed.git_gutter',
+      settings: {
+        foreground: '#F7B83D',
+      },
+    },
+    {
+      scope: 'meta.template.expression',
+      settings: {
+        foreground: '#FFFFFF',
+      },
+    },
+    {
+      scope: 'token.info-token',
+      settings: {
+        foreground: '#6796E6',
+      },
+    },
+    {
+      scope: 'token.warn-token',
+      settings: {
+        foreground: '#CD9731',
+      },
+    },
+    {
+      scope: 'token.error-token',
+      settings: {
+        foreground: '#F44747',
+      },
+    },
+    {
+      scope: 'token.debug-token',
+      settings: {
+        foreground: '#B267E6',
+      },
+    },
+  ],
+});
+
+export { rainglowAzureContrast as default };
