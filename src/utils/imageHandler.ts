2b2d8a4283066534b1a857f6200a4a673b093658
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-07-16 01:49:56 -0400

    fix(image-handling): add commented code to properly resolve image promise

--- src/utils/imageHandler.ts
+++ src/utils/imageHandler.ts
@@ -4,6 +4,16 @@ export const images = import.meta.glob<{ default: ImageMetadata }>(
   '/src/images/*.{jpeg,jpg,png,gif}'
 );
 
+// const resolvedImages = await Promise.all(
+//   Object.entries(images).map(async ([key, value]) => [
+//     key.split('/').pop(),
+//     (await value()).default,
+//   ])
+// );
+// export const imagesMap = Object.fromEntries(resolvedImages);
+
+//////////////////////////////////////////////
+
 // export function validateImage(data: any) {
 //   if (data.image && !images[data.image.url]) {
 //     throw new Error(`"${data.image.url}" does not exist in images directory"`);
