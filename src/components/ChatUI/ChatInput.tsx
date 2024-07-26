c31c36fbae6023b712f522f9a64cc990c7c10fe8
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-07-26 00:17:42 -0400

    chore(ChatInput): Re-add effect to input, move ref to input component

--- src/components/ChatUI/ChatInput.tsx
+++ src/components/ChatUI/ChatInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PaperPlaneIcon } from '@radix-ui/react-icons';
 import { Square } from 'lucide-react';
 import { Button } from '@components/ui/button';
@@ -12,11 +12,10 @@ interface ChatInputProps {
   input: UseChatHelpers['input'];
   isLoading: UseChatHelpers['isLoading'];
   stop: UseChatHelpers['stop'];
-  textareaRef: React.RefObject<HTMLTextAreaElement>;
 }
+
 export const ChatInput: React.FC<ChatInputProps> = ({
   handleSubmit,
-  textareaRef,
   input,
   isLoading,
   stop,
@@ -24,6 +23,8 @@ export const ChatInput: React.FC<ChatInputProps> = ({
 }) => {
   const disabled = !isLoading && input.length === 0;
   const formRef = React.useRef<HTMLFormElement>(null);
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.key === 'Enter' && !event.shiftKey) {
       event.preventDefault();
@@ -33,39 +34,41 @@ export const ChatInput: React.FC<ChatInputProps> = ({
     }
   };
 
-  if (textareaRef.current) {
-    textareaRef.current.style.height = 'auto';
-    textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-  }
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [input]);
 
   return (
     <form
       onSubmit={handleSubmit}
       ref={formRef}
-      className="relative flex w-full items-center">
+      className='relative flex w-full items-center'>
       <Textarea
         ref={textareaRef}
-        id="input"
-        name="prompt"
+        id='input'
+        name='prompt'
         rows={1}
-        placeholder="Message GPT"
-        className="max-h-52 flex-1 resize-none rounded-xl py-4 pr-10 text-[1rem] focus-visible:ring-ring/20"
-        autoComplete="off"
+        placeholder='Message GPT'
+        className='max-h-52 flex-1 resize-none rounded-xl py-4 pr-10 text-[1rem] focus-visible:ring-ring/20'
+        autoComplete='off'
         value={input}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
       />
       <Button
-        type="submit"
-        size="icon"
-        variant="secondary"
+        type='submit'
+        size='icon'
+        variant='secondary'
         disabled={disabled}
         onClick={isLoading ? stop : undefined}
         className={cn(
           'absolute bottom-3 right-3 transition-colors duration-200 ease-out [&_svg]:size-[17px]'
         )}>
         {isLoading ? <Square /> : <PaperPlaneIcon />}
-        <span className="sr-only">Send</span>
+        <span className='sr-only'>Send</span>
       </Button>
     </form>
   );
