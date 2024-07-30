d90033e39eda48981c8198584bb86fc89093b2f0
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-07-30 00:48:14 -0400

    fix: store created highlighters per language, fix importing all on load

--- src/lib/hooks/useShiki.ts
+++ src/lib/hooks/useShiki.ts
@@ -1,37 +1,57 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, type ReactNode } from 'react';
 import {
   createHighlighter,
-  type Highlighter,
   type BundledLanguage,
   type BundledTheme,
-  bundledLanguages,
+  type Highlighter,
 } from 'shiki';
+import parse from 'html-react-parser';
+import { removeTabIndexFromPre } from '@/lib/utils';
 
-const highlighter: Promise<Highlighter> = createHighlighter({
-  themes: [],
-  langs: Object.keys(bundledLanguages) as BundledLanguage[],
-});
+// This will store all created highlighter instances
+const highlighters = new Map<BundledLanguage, Promise<Highlighter>>();
 
 export const useShikiHighlighter = (
-  language: BundledLanguage,
+  lang: BundledLanguage | undefined,
   code: string,
-  theme: BundledTheme
+  theme: {
+    name: BundledTheme;
+    displayName: string;
+  }
 ) => {
-  const [highlightedCode, setHighlightedCode] = useState<string | null>(null);
+  const [highlightedCode, setHighlightedCode] = useState<ReactNode | null>(
+    null
+  );
 
   useEffect(() => {
-  const highlight = async () => {
-    const h = await highlighter;
-    const html = h.codeToHtml(code, {
-      lang: language,
-      theme,
-    });
-    setHighlightedCode(html);
-  };
-  // console.time('highlight');
-  highlight();
-  // console.timeEnd('highlight');
-}, [code]);
+    if (!lang) return;
+
+    const highlight = async () => {
+      if (!highlighters.has(lang)) {
+        // Create a new highlighter for this language
+        highlighters.set(
+          lang,
+          createHighlighter({
+            themes: [theme],
+            langs: [lang],
+          })
+        );
+      }
+
+      const highlighter = await highlighters.get(lang);
+
+      const html = highlighter?.codeToHtml(code, {
+        lang: lang as BundledLanguage,
+        theme: theme.name as BundledTheme,
+        transformers: [removeTabIndexFromPre],
+      });
+      // console.time('shiki');
+      setHighlightedCode(parse(html ? html : code));
+      // console.timeEnd('shiki');
+    };
+
+    highlight();
+  }, [code, lang]);
 
   return highlightedCode;
 };
