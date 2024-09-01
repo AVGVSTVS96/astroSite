97d59207bbffd05e309ce03cd37ab5548bf96105
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-09-01 02:47:14 -0400

    styling(prose):Update blog post code theme

--- astro.config.ts
+++ astro.config.ts
@@ -5,25 +5,17 @@ import react from '@astrojs/react';
 import icon from 'astro-icon';
 import expressiveCode, {
   type AstroExpressiveCodeOptions,
-  ExpressiveCodeTheme,
 } from 'astro-expressive-code';
-import fs from 'node:fs';
 import vercel from '@astrojs/vercel/serverless';
 
-const themeFile = fs.readFileSync(
-  new URL('./src/styles/rainglowAzure.jsonc', import.meta.url),
-  'utf-8'
-);
-const codeTheme = ExpressiveCodeTheme.fromJSONString(themeFile);
-
 const astroExpressiveCodeOptions: AstroExpressiveCodeOptions = {
-  themes: [codeTheme],
+  themes: ['github-dark-default'],
   themeCssSelector: (theme) => `.${theme.type}`,
   useThemedSelectionColors: true,
   styleOverrides: {
     textMarkers: {
-      markBackground: 'hsla(220, 25.00%, 30%, 0.4)',
-      markBorderColor: 'hsla(220, 30.00%, 30%, 1)',
+      markBackground: 'hsla(220, 25.00%, 30%, 0.25)',
+      markBorderColor: 'hsla(220, 30.00%, 60%, 1)',
     },
     frames: {
       inlineButtonBorderOpacity: '0.0',
