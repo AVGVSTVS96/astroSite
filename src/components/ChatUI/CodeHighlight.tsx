535fb459cf0edcf514da4025e95b03d7afbf8a9a
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-02-22 13:20:12 -0500

    Use both isinline and rehype inline plugin for some redundancy because why not

--- src/components/ChatUI/CodeHighlight.tsx
+++ src/components/ChatUI/CodeHighlight.tsx
@@ -7,16 +7,18 @@ interface CodeHighlightProps {
   className?: string | undefined;
   children?: ReactNode | undefined;
   node?: Element | undefined;
+  inline?: boolean | undefined;
 }
 
 export const CodeHighlight = ({
   className,
   children,
+  inline,
   node,
   ...props
 }: CodeHighlightProps) => {
-  const code = String(children);
-  const language = className?.match(/language-(\w+)/)?.[1];
+  const code = String(children).trim();
+  const language = className?.split('language-')?.[1];
 
   const highlightedCode = useShikiHighlighter(
     code,
@@ -27,7 +29,7 @@ export const CodeHighlight = ({
 
   const isInline = node && isInlineCode(node);
 
-  return !isInline ? (
+  return (!isInline || !inline) ? (
     <div className="shiki not-prose relative [&_pre]:overflow-auto [&_pre]:rounded-lg [&_pre]:px-6 [&_pre]:py-5">
       {language ? (
         <span className="absolute right-3 top-2 text-xs tracking-tighter text-muted-foreground/85">
