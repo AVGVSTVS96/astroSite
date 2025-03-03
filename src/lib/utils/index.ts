a124cdb8fbb9b1096ff4866b6b0df501006b8ce6
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-03-03 17:30:47 +0530

    fix

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,7 +1,5 @@
 export * from './cn';
 export * from './formatDate';
-export * from './isInlineCode';
 export * from './imageHandler';
-export * from './shikiTransformers';
 export * from './isActiveLink';
 export * from './toggleTheme';
