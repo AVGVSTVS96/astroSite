5b69fbafaa320ec7b54c70d73771f215a38dce7b
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2026-03-01 17:23:10 -0800

    feat: update projects on homepage

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,11 +3,13 @@ import { z, defineCollection } from 'astro:content';
 const projectsCollection = defineCollection({
   type: 'data',
   schema: z.object({
+    order: z.number().int().positive(),
     title: z.string(),
     description: z.string(),
     tags: z.array(z.string()),
     package: z.boolean().optional(),
     url: z.string().url(),
+    liveUrl: z.string().url().optional(),
   }),
 });
 
